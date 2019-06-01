e5c70b00f81300d69cd3f8fd3d5ac26a962ce76e
sisisin <azsisisin@gmail.com>
2019-06-01 21:15:26 +0900

    take props to Button

--- src/main/components/Button.tsx
+++ src/main/components/Button.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-export const Button = () => {
-  return <button>hoge!</button>;
+export const Button = (props: any) => {
+  return <button {...props}>hoge!</button>;
 };
